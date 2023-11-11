78e320a32e20db6a91a2ba218fe63295a07b1d54
Iliya Iliev <iliq123123@gmail.com>
2023-11-11 15:35:37 +0200

    Refactored and fixed edge cases. Also some comments added and minor clean-up

--- Радио Предаватели - Курсова работа/main.py
+++ Радио Предаватели - Курсова работа/main.py
@@ -1,37 +1,49 @@
-def main():
-    # the buildings count is useless so we ignore it
-    transmitter_range = int(input().split()[1])
-    buildings_positions = {int(position) for position in input().split()}
-
-    buildings_map: list[None | bool] = [None] * (max(buildings_positions) + 1)
-
-    # None means empty space, bool indicates if there is a radio transmitter
-    for position in buildings_positions:
-        buildings_map[position] = False
-
-    del buildings_positions
-
+def coverage(buildings_map: list[None | bool], transmitter_range: int) -> int:
+    transmitter_count = 0
     index = 0
+
+    # don't solve for empty map or if the transmitter range is negative
     while index < len(buildings_map) and transmitter_range >= 0:
         # check if there is a building
         if buildings_map[index] is False:
-            next_index = index + transmitter_range
+            proposed_transmitter_position = index + transmitter_range
 
             # check if the next transmitter position is a valid
-            while next_index >= len(buildings_map) or buildings_map[next_index] is None:
-                next_index -= 1
+            while (
+                proposed_transmitter_position >= len(buildings_map)
+                or buildings_map[proposed_transmitter_position] is None
+            ):
+                # if it is not decrement it
+                proposed_transmitter_position -= 1
 
             # put the transmitter
-            buildings_map[next_index] = True
+            buildings_map[proposed_transmitter_position] = True
 
-            # advance the position to the last building
-            # covered by the transmitter above
-            index += transmitter_range * 2
+            # set the index to the last building covered by the transmitter put above
+            index = proposed_transmitter_position + transmitter_range
+
+            # increase the transmitter count
+            transmitter_count += 1
 
         # advance the loop
         index += 1
 
-    print(sum(1 for transmitter in buildings_map if transmitter))
+    return transmitter_count
+
+
+def main():
+    # the buildings count at [0] is useless so we ignore it
+    transmitter_range = int(input().split()[1])
+    building_positions = {int(position) for position in input().split()}
+
+    # (+ 1) to include the 0th index
+    buildings_map: list[None | bool] = [None] * (max(building_positions) + 1)
+
+    # None means empty space, bool indicates if there is a radio transmitter
+    for position in building_positions:
+        buildings_map[position] = False
+
+    print(coverage(buildings_map, transmitter_range))
 
 
 if __name__ == "__main__":
